a9eb02227716d02c22426a87919a662262aa7388
Sarah <sarahvantilburg1@outlook.com>
2023-04-21 09:09:29 -0400

    Display the forecast data using the API

--- src/app.js
+++ src/app.js
@@ -15,31 +15,41 @@ function formatDate(timestamp){
     return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp){
+    let date =  new Date(timestamp * 1000);
+    let day = date.getDay()
+    let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+    return days[day];
+}
+
 function displayForecast(response) {
-    console.log(response.data.daily);
+    let forecast = response.data.daily;
+
+
     let forecastElement = document.querySelector("#forecast");
   
-    let days = ["Wed", "Thu", "Fri", "Sat", "Sun", "Mon"];
-  
+
     let forecastHTML = `<div class="row">`;
     // For loop, argument of function is day
-    days.forEach(function (day) {
+    forecast.forEach(function (forecastDay, index) {
+        if (index < 6){
       forecastHTML =
         forecastHTML +
         `
         <div class="col-2">
-          <div class="weather-forecast-date">${day}</div>
+          <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
           <img
-            src="http://openweathermap.org/img/wn/50d@2x.png"
+            src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png"
             alt=""
             width="42"
           />
           <div class="weather-forecast-temperatures">
-            <span class="weather-forecast-temperature-max"> 18° </span>
-            <span class="weather-forecast-temperature-min"> 12° </span>
+            <span class="weather-forecast-temperature-max"> ${Math.round(forecastDay.temp.max)}° </span>
+            <span class="weather-forecast-temperature-min"> ${Math.round(forecastDay.temp.min)}° </span>
           </div>
         </div>
         `;
+        }
     });
   
     forecastHTML = forecastHTML + `</div>`;
@@ -91,7 +101,7 @@ function search(city){
     axios.get(apiUrl).then(displayTemperature);
 }
 
-// Selecting city from search
+// Selecting city from search bar
 function handleSubmit(event){
     // doesn't reload page
     event.preventDefault();
